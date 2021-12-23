c9ec01ca796904f4505cc4e3548e8bfda4254ad0
longbem <giang.dinh@wrethink.com>
2021-12-23 11:05:53 +0700

    fix h4 to h2 in about

--- client/src/pages/about/index.js
+++ client/src/pages/about/index.js
@@ -12,9 +12,9 @@ function AboutPages() {
     <div className="container-about">
       <div className="col-6">
         <h1>Thông Tin</h1>
-        <h4>Lạc Thủy B Confessions</h4>
+        <h2>Lạc Thủy B Confessions</h2>
         <Info label="Địa chỉ" value="X.Phú Nghĩa - H.Lạc Thuỷ - T.Hoà Bình"/>
-        <Info label="Điện thoại" value="0123456789"/>
+        {/* <Info label="Điện thoại" value="0123456789"/> */}
         <Info label="Facebook" value={<a href="https://www.facebook.com/lacthuybconfessions" target="_blank" style={{color: 'blue'}} rel="noreferrer">Lạc Thủy B Confessions</a>}/>
       </div>
       <div className="col-6">
